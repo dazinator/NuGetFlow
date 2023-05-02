ecc32adb35152caf9b4f8e4efd65c41bdd7d5a79
Darrell Tunnell <darrell.tunnell@uniun.io>
2023-05-02 10:48:43 +0100

    Make usage of background hosted service optional.

--- src/NuGetFlow/ServiceCollectionExtensions.cs
+++ src/NuGetFlow/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 namespace Microsoft.Extensions.DependencyInjection;
+
 using Dazinator.Extensions.Options.ItemChanged;
 using NuGetFlow;
 using NuGetFlow.BackgroundTask;
@@ -8,6 +9,25 @@ public static class ServiceCollectionExtensions
 
     public static IServiceCollection AddNuGetPackageUpdater(this IServiceCollection services)
     {
+      
+
+        services.AddOptionsChangedMonitor<NuGetPackageInstallerOptions>();
+        services.AddSingleton<NuGetPackageInstaller>();
+        services.AddSingleton<IPackageOptionsHashProvider, PackageOptionsHashProvider>();
+        services.AddSingleton<IPackageHashStore, PackageHashStore>();
+        services.AddSingleton<INuGetPackagesOptionsInstallerService, NuGetPackagesOptionsInstallerService>();
+
+        return services;
+
+    }
+
+    /// <summary>
+    /// Adds an <see cref="IHostedService"/> that will run a task in the background ensure all configured nuget packages are installed on host start.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <remarks>Depends on .AddNuGetPackageUpdater to be called still, to register core services. </remarks>
+    public static IServiceCollection AddNuGetPackageUpdaterHostedService(this IServiceCollection services)
+    {
         services.AddHostedService<NuGetPackageUpdaterHostedService>();
         services.AddHostedService<QueuedHostedService>();
         services.AddSingleton<IBackgroundTaskQueue>(_ =>
@@ -15,20 +35,7 @@ public static class ServiceCollectionExtensions
             var queueCapacity = 15;
             return new DefaultBackgroundTaskQueue(queueCapacity);
         });
-
-        services.AddOptionsChangedMonitor<NuGetPackageInstallerOptions>();       
-        services.AddSingleton<NuGetPackageInstaller>();
-        services.AddSingleton<IPackageOptionsHashProvider, PackageOptionsHashProvider>();
-        services.AddSingleton<IPackageHashStore, PackageHashStore>();
-
-        services.AddSingleton<INuGetPackagesOptionsInstallerService, NuGetPackagesOptionsInstallerService>();
-
-        
-
-
-
         return services;
-
     }
 
 
